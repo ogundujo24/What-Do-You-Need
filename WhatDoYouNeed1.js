2e458ab7fc59481b88a1ec2e356a0b43433686e6
ogundujo24 <ogundujoshua@gmail.com>
2017-05-17 17:09:05 -0400

    Add files via upload

---
+++ WhatDoYouNeed1.js
@@ -0,0 +1,9 @@
+
+var submitBtn = document.getElementById("submitBtn");
+
+	function submitClick() {
+		var firebaseRef = firebase.database().ref();
+		var messageText = $(mainText).val()
+var mainText = document.getElementById("mainText").value;
+		firebaseRef.push().set(mainText);
+	}
